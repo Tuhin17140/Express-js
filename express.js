a7b153f505ee3b5df40e990186e2f2681b4f861c
Tuhin17140 <leoboy1007@gmail.com>
2021-08-03 13:15:22 +0600

    changes

--- express.js
+++ express.js
@@ -1,1 +1,34 @@
-console.log('Hello');
+// Express basics
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello World</h1>');
+})
+ 
+app.get('/about',(req,res)=>{
+    res.json({
+        name:'Tuhin'
+    });
+});
+
+app.post('/login',(req,res)=>{
+    console.log(req.body);
+
+    res.send('Login succesfull');
+})
+
+app.listen(3000,()=> console.log('Server is running at port 3000'));
+ 
